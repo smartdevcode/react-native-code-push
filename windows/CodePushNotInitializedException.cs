6f4970c75ddf0832051c3823efad8b54c7dec37b
SeniorDevCode <senior.dev.software@gmail.com>
2016-04-12 14:42:16 -0700

    update API

---
+++ windows/CodePushNotInitializedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodePush.ReactNative
+{
+    class CodePushNotInitializedException : Exception
+    {
+        public CodePushNotInitializedException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ windows/CodePushUnknownException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodePush.ReactNative
+{
+    class CodePushUnknownException : Exception
+    {
+        public CodePushUnknownException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
